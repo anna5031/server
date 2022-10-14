d87e5f3d7f74307c0c27af493ed44fd8f7729db9
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-10-14 12:24:22 +0900

    Solve merge conflict error

--- server.py
+++ server.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-from flask import Flask, request
-=======
 import flask
 import werkzeug
->>>>>>> main
  
 # Flask Constructor
 app = flask.Flask(__name__)
@@ -13,16 +9,6 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
       # response from the server
-<<<<<<< HEAD
-    return "This is home page"
-
-@app.route("/debug", methods=["POST"])
-def debug():
-    text = request.form["sample"]
-    print(text)
-    return "received"
-
-=======
   if flask.request.method=='POST':
     imagefile = flask.request.files['image']
     filename = werkzeug.utils.secure_filename(imagefile.filename)
@@ -32,6 +18,5 @@ def debug():
   else:
     return "Oh SHIT PLEASE GOD PLEASE LET ME GO HOME~~~"
  
->>>>>>> main
 if __name__ == "__main__":
   app.run(host="0.0.0.0")
