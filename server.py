247dfd8f7f872b05249fed17613c743b090d336b
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-10-14 12:19:19 +0900

    Make server download image from application

--- server.py
+++ server.py
@@ -1,14 +1,22 @@
-from flask import Flask
+import flask
+import werkzeug
  
 # Flask Constructor
-app = Flask(__name__)
+app = flask.Flask(__name__)
  
 # decorator to associate 
 # a function with the url
-@app.route("/")
-def showHomePage():
+@app.route('/', methods=['GET', 'POST'])
+def handle_request():
       # response from the server
-    return "This is home page1"
+  if flask.request.method=='POST':
+    imagefile = flask.request.files['image']
+    filename = werkzeug.utils.secure_filename(imagefile.filename)
+    print("\nReceived image File name : " + imagefile.filename)
+    imagefile.save(filename)
+    return "Image Uploaded Successfully"
+  else:
+    return "Oh SHIT PLEASE GOD PLEASE LET ME GO HOME~~~"
  
 if __name__ == "__main__":
   app.run(host="0.0.0.0")
